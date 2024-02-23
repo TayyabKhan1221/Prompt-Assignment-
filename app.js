b2a59071c276f6f231ab4668e3a53143dbe7c446
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-02-23 14:26:16 +0500

    prompt

---
+++ app.js
@@ -0,0 +1,25 @@
+let num1 = prompt("Enter First no.");
+let num2 = prompt("Enter Second no.");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+//Addition
+let sum = num1 + num2;
+alert("Addition: " + sum);
+console.log("Addition: " + sum);
+
+// //Subtraction
+// let mius = num1 - num2;
+// alert("Subtraction: " + minus);
+// console.log("Subtraction: " + minus);
+
+//Multiplication
+let multiply = num1 * num2;
+alert("Multiplication: " + multiply);
+console.log("Multiplication: " + multiply);
+
+//Division
+let divide = num1 / num2;
+alert("Division: " + divide);
+console.log("Division: " + divide);
